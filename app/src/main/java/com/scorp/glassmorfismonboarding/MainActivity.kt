edabc369ffddc85e9682c64debeb7a49fc1892a4
sardor-c <snspro777@gmail.com>
2026-01-04 11:18:00 +0500

    initial commit

---
+++ app/src/main/java/com/scorp/glassmorfismonboarding/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.scorp.glassmorfismonboarding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.scorp.glassmorfismonboarding.onboarding.presentation.components.BlurBackground
+import com.scorp.glassmorfismonboarding.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                BlurBackground()
+            }
+        }
+    }
+}
